04ffd0cd71204455a495b79459caedac81ae57a2
rbondar <rbondar@ip-10-0-92-176.eu-central-1.compute.internal>
2026-03-05 11:02:34 +0000

    Implemented error flag prediction in Scoreboard.

--- scoreboard.sv
+++ scoreboard.sv
@@ -19,23 +19,48 @@ class Scoreboard extends MonitorCallback;
 
     virtual function void write(Transaction tr);
         bit [3:0] predicted_result;
+        bit       predicted_err;
+        
         if (!configured) return;
 
-        case (tr.opcode)
-            3'd0: predicted_result = tr.a + tr.b; 
-            3'd1: predicted_result = tr.a - tr.b; 
-            3'd2: predicted_result = tr.a ^ tr.b; 
-            3'd3: predicted_result = tr.a & tr.b; 
-            3'd4: predicted_result = tr.a | tr.b; 
-            default: predicted_result = 0;
+        predicted_err = 0;
+            case (tr.opcode)
+    3'd0: begin // ADD
+        predicted_result = tr.a + tr.b;
+        if (tr.a + tr.b > 15) predicted_err = 1'b1;
+    end
+    3'd1: begin // SUB
+        predicted_result = tr.a - tr.b;
+        if (tr.a < tr.b) predicted_err = 1'b1;
+    end
+    3'd2: begin // XOR
+        predicted_result = tr.a ^ tr.b;
+        if (predicted_result == 4'd0) predicted_err = 1'b1;
+    end
+    3'd3: begin // AND
+        predicted_result = tr.a & tr.b;
+        if (predicted_result == 4'b1111) predicted_err = 1'b1;
+    end
+    3'd4: begin // OR
+        predicted_result = tr.a | tr.b;
+        if (predicted_result == 4'd0) predicted_err = 1'b1;
+    end
+    default: begin
+        predicted_result = 4'd0;
+        predicted_err = 1'b1;
+    end
         endcase
 
-        if (tr.result == predicted_result) passed++;
-        else failed++;
+        if ((tr.result === predicted_result) && (tr.error === predicted_err)) begin
+            passed++;
+        end else begin
+            failed++;
+            $error("[SCOREBOARD] Mismatch! Op:%0d A:%0d B:%0d | Act:{Res:%0d, Err:%b} Exp:{Res:%0d, Err:%b}",
+                    tr.opcode, tr.a, tr.b, tr.result, tr.error, predicted_result, predicted_err);
+        end
     endfunction
 
     function void report(string test_name = "General Analysis");
-        // Викликаємо функцію з utils.sv і передаємо їй ім'я
         print_test_summary(passed, failed, test_name);
     endfunction
 
